2924672b89ece259cb6ed05441c042d6a2387088
JamieShearer <31458526+JamieShearer@users.noreply.github.com>
2017-09-04 09:25:52 -0400

    Lab01 and Lab01ExtraCredit files

---
+++ Lab01ExtraCredit/Lab01ExtraCredit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab01ExtraCredit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello World"; // variable that stores the string "Hello World"
+            string name = "Jamie Shearer"; // variable that stores the name "Jamie Shearer"
+            Console.WriteLine($"{greeting} from {name}!");
+        }
+    }
+}
