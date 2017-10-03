8b42a436003c7ade0b94e57ffd688f4ae000d336
JamieShearer <31458526+JamieShearer@users.noreply.github.com>
2017-10-02 23:38:15 -0400

    Lab05

---
+++ Lab05-JamieShearer/Lab05/Lab05/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab05
+{
+    class Dog
+    {
+        static void bark()
+        {
+            string name = "Fido";
+            Console.WriteLine("{0} is Barking...", name);
+        }
+
+        static void doTrick()
+        {
+            string trickName = "rollover trick";
+            string name = "Fido";
+            Console.WriteLine("{0} is so smart! {0} is doing a {1}", name, trickName);
+        }
+    }
+}
